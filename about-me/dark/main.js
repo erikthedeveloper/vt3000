5a31fd4afaa7bf724af7e50859050ec66631caa5
Dev on CentosDev <erikthedeveloper@gmail.com>
2014-01-18 15:21:47 +0000

    Created Light and Dark variations on the about-me page.

---
+++ about-me/dark/main.js
@@ -0,0 +1,35 @@
+
+function chartInit(canvas_el) {
+    data = [
+        {
+            value : canvas_el.dataset.experience * 100,
+            // teal-ish
+            /* original */
+            // color:"#4FC2C5"
+
+            /* for dark */
+            color:"#007EE8"
+
+            /* for light */
+            // color:"#FF7600"
+        },
+        {
+            value : canvas_el.dataset.excitement * 100,
+            // pink-ish
+            // color : "#d24d57"
+            color : "transparent"
+        }
+    ];
+
+    var options = [];
+
+    var ctx = canvas_el.getContext("2d");
+    new Chart(ctx).Doughnut(data,options);
+}
+window.onload = function () {
+	var skills = document.getElementsByClassName('chart-canvas');
+
+	for (var i = skills.length - 1; i >= 0; i--) {
+		chartInit(skills[i]);
+	}
+}
---
+++ about-me/light/README.md
@@ -0,0 +1,11 @@
+# erikaybar.name
+
+As of now: nothing special. Word of wisdom to any lost soul who stumbles upon this: until this message is gone, I would advise you not to risk burning your eyes looking at the messy combobulation of code which is this repo.
+
+*Note to future: will be open-sourced personal website/blog built around Laravel 4.x and leveraging fancy things such as AngularJS and whatever other exciting things have my attention at the moment.
+
+*Note to future future self: Will house (hopefully) helpful tutorials for souls such as myself on the journey for wisdom. Wisdom of developing for the web.
+
+Again, disregard both the code and this message. 
+
+Goodbye.
