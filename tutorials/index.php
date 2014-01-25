f7229e6d29f96334b315b8a4bb27b01ac00aae78
Dev on CentosDev <erikthedeveloper@gmail.com>
2014-01-25 22:01:27 +0000

    MAJOR update... moved to HTML5 Boilerplate.

---
+++ tutorials/index.php
@@ -0,0 +1,71 @@
+<?php include_once 'includes/header.php'; ?>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+  <div class="container">
+    <h1>Concept Presentation 1</h1>
+    <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+  </div>
+</div>
+
+<div class="container">
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="col-md-6">
+      <h2>Heading</h2>
+      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+      <pre>
+        <xmp class="prettyprint">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+        </xmp>
+      </pre>
+
+      <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+    </div>
+
+    <div class="col-md-6">
+      <h2>Heading</h2>
+      <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+      <pre>
+        <code class="prettyprint">
+        body {
+          color: #f8f8f2;
+          text-shadow: 0 1px rgba(0,0,0,0.3);
+          font-family: Consolas, Monaco, 'Andale Mono', monospace;
+          direction: ltr;
+          text-align: left;
+          white-space: pre;
+          word-spacing: normal;
+          word-break: normal;
+
+          -moz-tab-size: 4;
+          -o-tab-size: 4;
+          tab-size: 4;
+
+          -webkit-hyphens: none;
+          -moz-hyphens: none;
+          -ms-hyphens: none;
+          hyphens: none;
+        }
+        </code>
+      </pre>
+
+      <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+   </div>
+
+  </div>
+</div>
+
+
+
+<?php include_once 'includes/footer.php'; ?>
