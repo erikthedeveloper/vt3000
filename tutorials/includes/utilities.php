fc9afc6b9af26bfd2b979c17a444a9139f2c9682
Dev on CentosDev <erikthedeveloper@gmail.com>
2014-01-27 03:02:31 +0000

    Added utilities.php

---
+++ tutorials/includes/utilities.php
@@ -0,0 +1,26 @@
+<?php
+
+$pieces = array(
+    'w-king'   => '&#9812;',
+    'w-queen'  => '&#9813;',
+    'w-rook'   => '&#9814;',
+    'w-bishop' => '&#9815;',
+    'w-knight' => '&#9816;',
+    'w-pawn'   => '&#9817;',
+    'b-king'   => '&#9818;',
+    'b-queen'  => '&#9819;',
+    'b-rook'   => '&#9820;',
+    'b-bishop' => '&#9821;',
+    'b-knight' => '&#9822;',
+    'b-pawn'   => '&#9823;'
+);
+$rows = array(
+    array('w-rook', 'w-knight', 'w-bishop', 'w-king', 'w-queen', 'w-bishop', 'w-knight', 'w-rook'),
+    array('w-pawn', 'w-pawn', 'w-pawn', 'w-pawn', 'w-pawn', 'w-pawn', 'w-pawn', 'w-pawn'),
+    array('', '', '', '', '', '', '', ''),
+    array('', '', '', '', '', '', '', ''),
+    array('', '', '', '', '', '', '', ''),
+    array('', '', '', '', '', '', '', ''),
+    array('b-pawn', 'b-pawn', 'b-pawn', 'b-pawn', 'b-pawn', 'b-pawn', 'b-pawn', 'b-pawn'),
+    array('b-rook', 'b-knight', 'b-bishop', 'b-king', 'b-queen', 'b-bishop', 'b-knight', 'b-rook'),
+    );
